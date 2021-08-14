12fab37c2f4824da2f4a54eae298604c3f6c9e6d
Justanewguy21 <taothanh2020@gmail.com>
2021-08-14 17:25:16 +0700

    start learning with interger operator

---
+++ bai2_varint.py
@@ -0,0 +1,38 @@
+# ------------------------------------------------------
+# By    Tao Thanh
+# Date  12 Aug 2021
+# Todo  Work with var, interger data type and basic operators
+# 12/8/2021
+#   - learning about int             
+# ------------------------------------------------------
+# declair variable basic operators
+x = 8
+y = 2
+print(x + y)
+print(x * y)
+print(x / y)
+print(x - y)
+x = 7
+print(x // y)
+print(x % y)
+y = 3
+print(x ** y) # x**y = 7^3
+x += 3 # X += 3 also means x = x +3
+print(x)
+x *= 3
+print(x)
+x /= 3
+print(x)
+x -= 3 
+print(x)
+
+# float/deciam
+f = 5.5
+f += 3
+print(f)
+f -= 3
+print(f)
+f *= 3
+print(f)
+f /= 3
+print(f)
