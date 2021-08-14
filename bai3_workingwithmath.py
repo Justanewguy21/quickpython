f875c336ef897b108cbd3b56148e812213de1eae
Justanewguy21 <taothanh2020@gmail.com>
2021-08-14 17:40:35 +0700

    workingwithmathfunction

---
+++ bai3_workingwithmath.py
@@ -0,0 +1,28 @@
+# ------------------------------------------------------
+# By    Tao Thanh
+# Date  12 Aug 2021
+# Todo  Work with Math module
+# 12/8/2021
+#   - continue with floor, ceil, sqrt, sin, cos, tan, cot             
+# ------------------------------------------------------
+# import math module to use math functions in python
+import math
+#  CEIL function
+f = 23.85
+print(f)
+print(math.ceil(f))
+f = 23.25
+print(f)
+print(math.ceil(f))
+
+# FLOOR FUNCTION
+print("math.floor function")
+f = 23.85
+print(f)
+print(math.floor(f))
+
+#  sin, cos, tan
+print(math.sin(45))
+
+# sqrt
+print(math.sqrt(4))
