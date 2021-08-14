cc2f47644db7396207878cb161ff3022c583de82
Justanewguy21 <taothanh2020@gmail.com>
2021-08-14 16:57:21 +0700

    still learn about string

--- bai1_varstr.py
+++ bai1_varstr.py
@@ -1,11 +1,30 @@
-#By Tao Thanh, 2021Aug11
-#Todo: Lear pythong string variables and function
-valstr = "welcome to the python"
-print(valstr)
-#String concatenation
-name = "Thanh"
-print("My nam is " + name)
-name = 1
-print("gia tri cua name la " + str(name))
-#dong lenh thu 8,9
-
+# ------------------------------------------------------
+# By    Tao Thanh
+# Date  12 Aug 2021
+# Todo  Work with var and string data type
+# 12/8/2021
+#   - continue with string             
+# ------------------------------------------------------
+# declair a string variable 
+s = "start learning python"
+print(s)
+# command concatenation
+name_21 = "Ben"
+print("welcome " + name_21)
+name_21 = 68149
+print("welcome " + str(name_21))
+# str basic functions
+name_21 = "Learning python is quite interesting"
+print("length of name_21 is " + str(len(name_21)))
+#  variable.find(str that need to find) -> If it find out, it will return a int >= 0
+print(name_21.find("Python"))
+# upper and lower // isupper and islower
+if name_21.islower():
+    print('1')
+    print(name_21.upper())
+else:
+    print('2')
+    print (name_21.upper())
+name_21 = "Bee"
+age = 6
+print(f'{name_21} is {age}')
